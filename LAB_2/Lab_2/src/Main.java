79097e57e073a4cb0c1ad63ad3b772039baa3cad
Mrsaskas <95799385+Mrsaskas@users.noreply.github.com>
2023-09-14 19:23:02 +0300

    Add files via upload

---
+++ LAB_2/Lab_2/src/Main.java
@@ -0,0 +1,54 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // 20 Version
+
+        // Task 1
+        // 10 case
+
+        double a = 1.27;
+        double b = 10.99;
+        double c = 2.73;
+        double d = 25.32;
+
+        double y  = (Math.pow(a,b)/Math.sinh(Math.abs(b)))+(4* Math.log10(c)/Math.pow(d,4));
+
+
+        System.out.println("\n"+y);
+
+        // Task 2
+        // 20 case
+
+        double a2 = 3.56;
+        double b2 = 1.02;
+        double c2 = 3;
+        double d2 = 2.43;
+
+        double y2  = 2 * Math.log10(Math.pow(b2,a2)) + Math.abs(Math.asin((Math.sqrt(d2/c2)) * -1));
+
+        System.out.println("\n"+y2);
+
+        // Task 2
+        // 30 case
+
+        double a3 = 3.56;
+        double b3 = 1.02;
+        double c3 = 3;
+        double d3 = 2.43;
+
+        double y3  = (3*a/Math.cos(a)) + (Math.sqrt((Math.tanh(Math.abs(b) * c))/ Math.log10(d)));
+
+        System.out.println("\n"+y3);
+
+
+
+
+
+
+
+
+
+    }
+
+}
